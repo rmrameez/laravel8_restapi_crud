2765ce807187266d43cb152a780cc8972a79c437
Rameez <m_rameez@hotmail.com>
2021-05-04 23:53:42 +0500

    Restful api crud

---
+++ routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('/products',[ProductController::class,'index']);
+Route::post('/products',[ProductController::class,'store']);
+Route::put('/products/{id}',[ProductController::class,'update']);
+Route::delete('/products/{id}',[ProductController::class,'destroy']);
+Route::get('/products/{id}',[ProductController::class,'show']);
